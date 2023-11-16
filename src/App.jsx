467bbbd107348229f4386b615a265b0f4a45d773
stevkim <stevenn.kkim@gmail.com>
2023-11-15 18:05:38 -0800

    Added feature for star-rating. Required a little algo to convert to the nearest quarter. Tests created and ran.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import StarRating from './components/star-rating/StarRating.jsx';
 
 const App = () => {
   return (
     <div>
-      Testing!
-      Stevkim
+      <StarRating rating={3.2} />
     </div>
   )
 }

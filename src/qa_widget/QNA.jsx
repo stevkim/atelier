68de0c8c77e4d59bec55bcd094a5b241ae587804
hazelkimberly <hazelkimc@gmail.com>
2023-11-16 16:15:19 -0800

    Created a parent component (QNA) and child component (QuestionList)

---
+++ src/qa_widget/QNA.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import QuestionList from './QuestionList.jsx';
+
+export default function QNA() {
+  return (
+    <div>
+
+      <h4>QUESTIONS AND ANSWERS</h4>
+
+      <QuestionList />
+
+    </div>
+  );
+};

fc39139ab58b058ea2adeb121737ee6e8f15d409
hazelkimberly <hazelkimc@gmail.com>
2023-11-16 17:06:52 -0800

    Begin building out the QuestionList component

--- src/qa_widget/QNA.jsx
+++ src/qa_widget/QNA.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import QuestionList from './QuestionList.jsx';
+import { questions } from './sampleData.js';
 
 export default function QNA() {
+  const [questionList, setQuestionList] = useState([]);
+
+  // Keep in mind that by default there should only be 2 questions displayed
+  // in the order of most helpful to least helpful
+  useEffect(() => {
+    setQuestionList(questions.results);
+  }, [])
+
+  // If there are no questions, only display add a question button
   return (
     <div>
-
       <h4>QUESTIONS AND ANSWERS</h4>
-
-      <QuestionList />
-
+      {questions.length > 0 && <QuestionList questionList={questionList}/>}
     </div>
   );
 };

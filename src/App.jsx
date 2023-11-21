4d76edea3b048386b07bba30704a40e4509c37fb
Jose Felix <jmfelix195@gmail.com>
2023-11-21 09:35:39 -0700

    Add currentProduct state to track current product id

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionsAndAnswers from './qa/QuestionsAndAnswers.jsx';
 import Overview from './components/overview/Overview.jsx';
 import RatingsReviews from './ratings-reviews/RatingsReviews.jsx';
 import RelatedProductsAndComparison from './components/related-products-and-comparison/RelatedProductsAndComparison.jsx';
 
 const App = () => {
+  const [currentProduct, setCurrentProduct] = useState(40347);
+
   return (
     <div>
       <Overview />
-      <RelatedProductsAndComparison />
+      <RelatedProductsAndComparison currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} />
       <QuestionsAndAnswers />
       <RatingsReviews />
     </div>

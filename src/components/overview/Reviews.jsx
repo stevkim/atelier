c55b86f318b1682f647745ca56d97efed2ba967f
Gralayer <wisenathaniel1998@gmail.com>
2023-11-22 10:26:24 -0500

    Made the layout more resemble the business requirements.

--- src/components/overview/Reviews.jsx
+++ src/components/overview/Reviews.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import StarRating from '../star-rating/StarRating.jsx';
 
-const Reviews = ({ reviews }) => {
+const Reviews = ({ product }) => {
   var totalReviews = 0;
   var totalStars = 0;
-  for (var starsGiven in reviews) {
-    totalStars += (starsGiven * reviews[starsGiven]);
-    totalReviews += Number(reviews[starsGiven]);
+  for (var starsGiven in product.reviews) {
+    totalStars += (starsGiven * product.reviews[starsGiven]);
+    totalReviews += Number(product.reviews[starsGiven]);
   }
   return (
     <div className='overview-reviews'>

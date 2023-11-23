c3b962afa5e7d2adeaec903cbda9ac1ac4ea810b
hazelkimberly <hazelkimc@gmail.com>
2023-11-22 18:41:36 -0800

    Moved AnswerList into QuestionEntry and added QuestionList component into QuestionsAndAnswers

--- src/qa/QuestionsAndAnswers.jsx
+++ src/qa/QuestionsAndAnswers.jsx
@@ -1,73 +1,32 @@
 require('dotenv').config();
 import React, { useState, useEffect } from 'react';
-import AnswerList from './AnswerList.jsx';
-import QuestionEntry from './QuestionEntry.jsx';
+import QuestionList from './QuestionList.jsx';
 import axios from 'axios';
 import './qaStyles.css';
 
 export default function QuestionsAndAnswers() {
-  const [currAnswerList, setCurrAnswerList] = useState([]);
-  const [totalAnswers, setTotalAnswers] = useState(0);
-  const [count, setCount] = useState(2);
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [currQuestionList, setCurrQuestionList] = useState([]);
 
   const serverURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp';
   const headers = { Authorization: process.env.GIT_TOKEN };
 
   useEffect(() => {
-    axios.get(`${serverURL}/qa/questions`, {
-      headers: headers,
-      params: {
-        product_id: 40344
-      }
-    })
+    axios.get(`${serverURL}/qa/questions/?product_id=40348&count=50`, { headers: headers })
       .then((response) => {
-        const answerObj = response.data.results[0].answers;
-        setTotalAnswers(Object.keys(answerObj).length);
+        setCurrQuestionList(response.data.results);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  useEffect(() => {
-    axios.get(`${serverURL}/qa/questions/647006/answers`, {
-      headers: headers,
-      params: {
-        page: 1,
-        count: count
-      }
-    })
-      .then((response) => {
-        setCurrAnswerList(response.data.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }, [count]);
-
-  const handleLoadMoreAnswers = () => {
-    currAnswerList.length < totalAnswers && setCount(totalAnswers);
-    setIsExpanded(true);
-  };
-
-  const handleCollapseAnswers = () => {
-    currAnswerList.length === totalAnswers && setCount(2);
-    setIsExpanded(false);
-  };
-
   return (
     <div>
       <h4>QUESTIONS AND ANSWERS</h4>
-      <QuestionEntry />
-      <AnswerList
-        currAnswerList={ currAnswerList }
+      <QuestionList
+        currQuestionList={currQuestionList}
         serverURL={serverURL}
         headers={headers}
-        totalAnswers={totalAnswers}
-        handleLoadMoreAnswers={handleLoadMoreAnswers}
-        handleCollapseAnswers={handleCollapseAnswers}
-        isExpanded={isExpanded}
       />
     </div>
   )

77d71cee9011b5ca1592e9c67a0d8d3407e9bca6
hazelkimberly <hazelkimc@gmail.com>
2023-11-22 18:39:05 -0800

    Moved AnswerList from QuestionsAndAnswers into QuestionEntry and sorted the list of answers to make sure to show answers from sellers first regardless of its helpfulness count

--- src/qa/QuestionEntry.jsx
+++ src/qa/QuestionEntry.jsx
@@ -1,16 +1,94 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import AnswerList from './AnswerList.jsx';
 
-export default function QuestionEntry() {
+export default function QuestionEntry({ question, serverURL, headers }) {
+  const { question_id, question_body, question_helpfulness, answers } = question;
+  const [updateQuestionHelpfulness, setUpdateQuestionHelpfulness] = useState(question_helpfulness);
+  const [isQuestionHelpful, setIsQuestionHelpful] = useState(false);
+  const [currAnswerList, setCurrAnswerList] = useState([]);
+  const [answerList, setAnswerList] = useState([]);
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleHelpfulQuestionClick = (id) => {
+    !isQuestionHelpful &&
+    axios.put(`${serverURL}/qa/questions/${id}/helpful`, null, { headers: headers })
+      .then(() => {
+        setUpdateQuestionHelpfulness(updateQuestionHelpfulness + 1);
+        setIsQuestionHelpful(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const totalAnswers = Object.keys(answers).length;
+
+  const handleLoadMoreAnswers = () => {
+    setCurrAnswerList(answerList);
+    setIsExpanded(true);
+  };
+
+  const handleCollapseAnswers = () => {
+    setCurrAnswerList(answerList.slice(0, 2));
+    setIsExpanded(false);
+  };
+
+  useEffect(() => {
+    axios.get(`${serverURL}/qa/questions/${question_id}/answers/?count=${totalAnswers}`, { headers: headers })
+      .then((response) => {
+        const sortedAnswerList = response.data.results.sort((a,b) => {
+          const isSellerA = a.answerer_name === 'Seller';
+          const isSellerB = b.answerer_name === 'Seller';
+
+          if (isSellerA && !isSellerB) {
+            return -1;
+          } else if (!isSellerA && isSellerB) {
+            return 1;
+          } else {
+            return b.helpfulness - a.helpfulness;
+          }
+        })
+        setAnswerList(sortedAnswerList);
+        setCurrAnswerList(sortedAnswerList.slice(0, 2));
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
+
   return (
     <div className='question-container'>
-      <div className='question-indicator'>
-        <span style={{ fontWeight: 'bold' }}>Q: </span>
-        <span>What is the question here?</span>
+      <div className='question-header'>
+        <div className='question-indicator' style={{ fontWeight: 'bold' }}>
+          <div>Q:</div>
+          <div>{question_body}</div>
+        </div>
+        <div className='question-details-container'>
+          <span className='helpful-container'>
+            <span>Helpful?</span>
+            <span
+              className='yes'
+              style={{ textDecoration: isQuestionHelpful ? 'none' : 'underline', cursor: isQuestionHelpful && 'default'}}
+              onClick={() => {handleHelpfulQuestionClick(question_id)}}>
+              Yes
+            </span>
+            <span>({updateQuestionHelpfulness})</span>|
+            <span className='add-answer' style={{textDecoration: 'underline'}}>Add Answer</span>
+          </span>
+        </div>
       </div>
       <div className='answer-indicator'>
-        <span style={{ fontWeight: 'bold' }}>A: </span>
-
+        <div style={{ fontWeight: 'bold' }}>A:</div>
+        <AnswerList
+          currAnswerList={currAnswerList}
+          serverURL={serverURL}
+          headers={headers}
+          totalAnswers={totalAnswers}
+          handleLoadMoreAnswers={handleLoadMoreAnswers}
+          handleCollapseAnswers={handleCollapseAnswers}
+          isExpanded={isExpanded}
+        />
       </div>
     </div>
   )

3347d3d1de96053e31f63636937dbb3330ccf69c
Gralayer <wisenathaniel1998@gmail.com>
2023-11-24 12:25:35 -0500

    Made API calls consistent with the proxy server.

--- src/components/overview/helper-funcs/addToCart.js
+++ src/components/overview/helper-funcs/addToCart.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
-require('dotenv').config();
 
 const addToCart = (skuNum, quantity) => {
   let config = {
     method: 'post',
-    url: process.env.API_URL + 'cart?sku_id=' + skuNum,
-    headers: {
-      Authorization: process.env.GIT_TOKEN
-    }
+    url: '/cart?sku_id=' + skuNum,
   };
 
   axios.request(config)

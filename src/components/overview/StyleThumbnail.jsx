c3aca4111e6ac20715bf5a34e724e25d7d3aa594
Gralayer <wisenathaniel1998@gmail.com>
2023-11-24 05:35:08 -0500

    Replaced the image of a checkmark with a real checkmark.

--- src/components/overview/StyleThumbnail.jsx
+++ src/components/overview/StyleThumbnail.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const StyleThumbnail = ({ url, isSelected, updateStyle, index }) => {
-  return (
-    <img className='overview-style-thumbnail' onClick={() => { updateStyle(index) }}
-      src={isSelected
-        ? 'https://th.bing.com/th/id/OIP.7Vo3_7R0oiUNmKPPK6CgyQHaG1?rs=1&pid=ImgDetMain'
-        : url} />
-  );
+  if (isSelected) {
+    return (
+      <div className='overview-style-thumbnail'>&#10004;</div>
+    );
+  } else {
+    return (
+      <img className='overview-style-thumbnail' onClick={() => { updateStyle(index) }} src={url} />
+    );
+  }
 }
 
 export default StyleThumbnail;
--- src/components/overview/Thumbnail.jsx
+++ src/components/overview/Thumbnail.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Thumbnail = ({url, isSelected, updateThumbnail, index}) => {
-  return (
-    <img className='overview-thumbnail'
-      onClick={() => {updateThumbnail(index)}}
-      src={isSelected
-      ? 'https://th.bing.com/th/id/OIP.7Vo3_7R0oiUNmKPPK6CgyQHaG1?rs=1&pid=ImgDetMain'
-      : url} />
-  );
+  if (isSelected) {
+    return (
+      <div className='overview-thumbnail'>&#10004;</div>
+    );
+  } else {
+    return (
+      <img className='overview-thumbnail' onClick={() => { updateThumbnail(index) }} src={url} />
+    );
+  }
 }
 
 export default Thumbnail;

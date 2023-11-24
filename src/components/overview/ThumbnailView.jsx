d2693abc39cbaadfc25bb64cfcd6894d34d45f08
Gralayer <wisenathaniel1998@gmail.com>
2023-11-24 01:47:40 -0500

    Added a 'down' button to the main image thumbnail view. Limited the thumbnail view to 5 images, which rotate as the user clicks the 'down' button.

--- src/components/overview/ThumbnailView.jsx
+++ src/components/overview/ThumbnailView.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Thumbnail from './Thumbnail.jsx';
 
-const ThumbnailView = ({thumbnails, thumbnail, updateThumbnail}) => {
+const ThumbnailView = ({ thumbnails, thumbnail, updateThumbnail }) => {
+  const [topThumbnailIndex, setTopThumbnailIndex] = useState(0);
+
+  useEffect(() => {
+    updateThumbnail(topThumbnailIndex);
+  }, [topThumbnailIndex])
+
+  const changeThumbnailIndex = () => {
+    if (topThumbnailIndex + 1 === thumbnails.length) {
+      setTopThumbnailIndex(0);
+    } else {
+      setTopThumbnailIndex(topThumbnailIndex + 1);
+    }
+  };
+
+  const rotatingSliceOf5 = (arr) => {
+    let sliceOf5 = arr.slice(topThumbnailIndex, Math.min(topThumbnailIndex + 5, thumbnails.length))
+    for (var i = 0; sliceOf5.length < Math.min(5, arr.length); i++) {
+      sliceOf5.push(arr[i]);
+    }
+    return sliceOf5;
+  }
+
   return (
     <div className='overview-thumbnail-view'>
-      {thumbnails.slice(0, 5).map((photo, index) => {
+      {rotatingSliceOf5(thumbnails).map((photo, index) => {
         return (
           <Thumbnail key={'overview-thumbnail-' + index}
-          thumbnail={thumbnail}
-          updateThumbnail={updateThumbnail}
-          url={photo.thumbnail_url}
-          index={index} />
+            thumbnail={thumbnail}
+            updateThumbnail={updateThumbnail}
+            url={photo.thumbnail_url}
+            index={index} />
         );
       })}
+      <button
+        className='overview-thumbnail-button'
+        onClick={() => { changeThumbnailIndex() }}
+      > DOWN </button>
     </div>
   );
 }

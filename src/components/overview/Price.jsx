a0ff14e41e2a5490e88760d39935cd6e4985f8e0
Gralayer <wisenathaniel1998@gmail.com>
2023-11-24 04:14:12 -0500

    Set regular price to turn red and be struck-through when a product is on-sale.

--- src/components/overview/Price.jsx
+++ src/components/overview/Price.jsx
@@ -6,9 +6,9 @@ const Price = ({ selectedStyle }) => {
   const isOnSale = salePrice > 0;
   return (
     <div className='overview-price-container'>
-      <span className='overview-price'>${regularPrice}</span>
+      <span className={'overview-price-' + isOnSale}>${regularPrice}</span>
       {isOnSale
-        ? <span className='overview-sale-price'>{' Sale: $' + salePrice}</span>
+        ? <span className='overview-sale-price'>{' $' + salePrice}</span>
         : ''}
       <div className='overview-selected-style'>
         {selectedStyle.name}

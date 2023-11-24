fe147899e5e63b2af8d425f479d2a56f9bb8defc
Gralayer <wisenathaniel1998@gmail.com>
2023-11-24 00:31:38 -0500

    Made it so only 5 thumbnails are rendered at a time.

--- src/components/overview/ThumbnailView.jsx
+++ src/components/overview/ThumbnailView.jsx
@@ -4,7 +4,7 @@ import Thumbnail from './Thumbnail.jsx';
 const ThumbnailView = ({thumbnails, thumbnail, updateThumbnail}) => {
   return (
     <div className='overview-thumbnail-view'>
-      {thumbnails.map((photo, index) => {
+      {thumbnails.slice(0, 5).map((photo, index) => {
         return (
           <Thumbnail key={'overview-thumbnail-' + index}
           thumbnail={thumbnail}

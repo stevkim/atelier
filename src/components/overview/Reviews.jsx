e8122cf23673fd3d8801b7c2477eef91203a7ad2
Gralayer <wisenathaniel1998@gmail.com>
2023-11-24 12:48:22 -0500

    Changed for loop in reviews.jsx into a useEffect

--- src/components/overview/Reviews.jsx
+++ src/components/overview/Reviews.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import StarRating from '../star-rating/StarRating.jsx';
 
 const Reviews = ({ product }) => {
-  var totalReviews = 0;
-  var totalStars = 0;
-  for (var starsGiven in product.reviews) {
-    totalStars += (starsGiven * product.reviews[starsGiven]);
-    totalReviews += Number(product.reviews[starsGiven]);
-  }
+  let totalReviews = 0;
+  let totalStars = 0;
+
+  useEffect(() => {
+    for (var starsGiven in product.reviews) {
+      totalStars += (starsGiven * product.reviews[starsGiven]);
+      totalReviews += Number(product.reviews[starsGiven]);
+    }
+  }, [product]);
+
   return (
     <div className='overview-reviews'>
       < StarRating rating={totalStars / totalReviews} />

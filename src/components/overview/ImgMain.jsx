717dd7ee63d5ad224031be1c43169145a9828041
Gralayer <wisenathaniel1998@gmail.com>
2023-11-27 12:40:46 -0500

    Made the details section smaller.

--- src/components/overview/ImgMain.jsx
+++ src/components/overview/ImgMain.jsx
@@ -17,23 +17,24 @@ const ImgMain = ({ url, expanded, changeView }) => {
         onClick={() => {
           if (!expanded) {
             container.style.transform = `scale(1, 1.125)`;
-            container.style.paddingTop = '8%';
+            container.style.paddingTop = '4%';
             container.style.paddingBottom = '3%';
             container.style.marginBottom = '8%';
-            thisImg.style.transform = 'scale(2, 1.125)';
+            thisImg.style.transform = 'scale(2, 1)';
             thisImg.style.translate = '40% 0%';
             changeView();
           } else {
             container.addEventListener('mousemove', (e) => {
               // Set up relative zooming
               const rect = thisImg.getBoundingClientRect();
-              const scaleX = thisImg.offsetWidth / rect.width; // zoom power
-              const scaleY = thisImg.offsetHeight / rect.height;
+              console.log(thisImg.offsetWidth, rect.width, rect.left);
+              const scaleX = thisImg.offsetWidth / (rect.width); // zoom power
+              const scaleY = thisImg.offsetHeight / (rect.height);
               const mouseX = e.clientX;
               const mouseY = e.clientY;
 
               // Set up zoom box position and size
-              const zoomSize = 100;
+              const zoomSize = 200;
               const zoomRectangleX = mouseX - zoomSize / 2;
               const zoomRectangleY = mouseY - zoomSize / 2;
 
@@ -42,11 +43,12 @@ const ImgMain = ({ url, expanded, changeView }) => {
               zoomBox.style.height = zoomSize + 'px';
               // zoomBox.style.transform = `scale(${scaleX}, ${scaleY})`;
               zoomBox.style.backgroundImage = thisImg.style.backgroundImage;
-              zoomBox.style.backgroundSize = `${rect.width * scaleX}px ${rect.height * scaleY}px`;
-              zoomBox.style.backgroundPosition = `-${mouseX * scaleX}px -${mouseY * scaleY}px`;
+              zoomBox.style.backgroundSize = `${rect.right * scaleX}px ${rect.height * scaleY}px`;
+              zoomBox.style.backgroundPosition = `-${(mouseX - 2 * rect.left) * scaleX}px -${(mouseY - 2 * rect.top) * scaleY}px`;
               zoomBox.style.left = zoomRectangleX + 'px';
               zoomBox.style.top = zoomRectangleY + 'px';
               zoomBox.style.display = 'block';
+              zoomBox.style.objectFit = 'fill';
             })
             thisImg.addEventListener('mouseenter', () => {
               zoomBox.style.display = 'block';

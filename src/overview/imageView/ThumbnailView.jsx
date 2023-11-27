355146163fa7aab50f0d72d8b4d1fff0c8f88c07
Gralayer <wisenathaniel1998@gmail.com>
2023-11-27 18:45:06 -0500

    "Merge handling"

--- src/overview/imageView/ThumbnailView.jsx
+++ src/overview/imageView/ThumbnailView.jsx
@@ -1,11 +1,7 @@
 import React, { useState, useEffect, expanded } from 'react';
 import Thumbnail from './Thumbnail.jsx';
 
-<<<<<<< HEAD:src/overview/imageView/ThumbnailView.jsx
-const ThumbnailView = ({ thumbnails, thumbnail, expanded, updateThumbnail, }) => {
-=======
-const ThumbnailView = ({ thumbnails, thumbnail, updateThumbnail }) => {
->>>>>>> b1d04ad926e3fd85c6578aaca8b85c5e4e5df630:src/components/overview/ThumbnailView.jsx
+const ThumbnailView = ({ thumbnails, thumbnail, expanded, updateThumbnail }) => {
   const [topThumbnailIndex, setTopThumbnailIndex] = useState(0);
   const sizeOfThumbnailView = 7;
 
@@ -32,15 +28,9 @@ const ThumbnailView = ({ thumbnails, thumbnail, updateThumbnail }) => {
     }
   };
 
-<<<<<<< HEAD:src/overview/imageView/ThumbnailView.jsx
   const rotatingSlice = (arr) => {
     let slice = arr.slice(topThumbnailIndex, Math.min(topThumbnailIndex + sizeOfThumbnailView, thumbnails.length))
     for (var i = 0; slice.length < Math.min(sizeOfThumbnailView, arr.length); i++) {
-=======
-  const rotatingSliceOf5 = (arr) => {
-    const slice = arr.slice(topThumbnailIndex, Math.min(topThumbnailIndex + sizeOfThumbnailView, thumbnails.length));
-    for (let i = 0; slice.length < Math.min(sizeOfThumbnailView, arr.length); i++) {
->>>>>>> b1d04ad926e3fd85c6578aaca8b85c5e4e5df630:src/components/overview/ThumbnailView.jsx
       slice.push(arr[i]);
     }
     return slice;
@@ -57,7 +47,6 @@ const ThumbnailView = ({ thumbnails, thumbnail, updateThumbnail }) => {
 
   return (
     <>
-<<<<<<< HEAD:src/overview/imageView/ThumbnailView.jsx
       <div className={'overview-thumbnail-view-' + expanded}>
         {rotatingSlice(thumbnails).map((photo, index) => {
           return (
@@ -69,18 +58,6 @@ const ThumbnailView = ({ thumbnails, thumbnail, updateThumbnail }) => {
               index={(index + topThumbnailIndex) % thumbnails.length} />
           );
         })}
-=======
-      <div className='overview-thumbnail-view'>
-        {rotatingSliceOf5(thumbnails).map((photo, index) => (
-          <Thumbnail
-            key={`overview-thumbnail-${index}`}
-            url={photo.thumbnail_url}
-            isSelected={(index + topThumbnailIndex) % thumbnails.length === thumbnail}
-            updateThumbnail={updateThumbnail}
-            index={(index + topThumbnailIndex) % thumbnails.length}
-          />
-        ))}
->>>>>>> b1d04ad926e3fd85c6578aaca8b85c5e4e5df630:src/components/overview/ThumbnailView.jsx
         <button
           className='overview-thumbnail-button'
           onClick={() => { incrementThumbnail(+1); }}
@@ -94,11 +71,6 @@ const ThumbnailView = ({ thumbnails, thumbnail, updateThumbnail }) => {
       <button className='overview-right-button' onClick={() => { incrementThumbnail(+1); }}>{'->'}</button>
     </>
   );
-<<<<<<< HEAD:src/overview/imageView/ThumbnailView.jsx
-
-}
-=======
 };
->>>>>>> b1d04ad926e3fd85c6578aaca8b85c5e4e5df630:src/components/overview/ThumbnailView.jsx
 
 export default ThumbnailView;

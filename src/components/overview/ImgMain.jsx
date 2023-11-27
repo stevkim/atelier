66d40a6f7e937c8c7c12dd518a9b442b5a52ee25
Gralayer <wisenathaniel1998@gmail.com>
2023-11-27 12:04:08 -0500

    Implemented zoom view, which moves with the mouse. However, the zoom window is offset by an amount I've yet to discover.

--- src/components/overview/ImgMain.jsx
+++ src/components/overview/ImgMain.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const ImgMain = ({ url, expanded, changeView }) => {
+  let thisImg = document.getElementById('overview-img-main');
+  let zoomBox = document.getElementById('zoom-box');
+  let container = document.getElementById('overview');
+
   return (
     <>
       <div
@@ -11,18 +15,50 @@ const ImgMain = ({ url, expanded, changeView }) => {
           backgroundSize: '100% 100%'
         }}
         onClick={() => {
-          let thisImg = document.getElementById('overview-img-main');
-          let container = document.getElementById('overview');
-          container.style.transform = `scale(1, 1.25)`;
-          container.style.paddingTop = '8%';
-          container.style.paddingBottom = '3%';
-          container.style.marginBottom = '8%';
-          thisImg.style.transform = 'scale(2, 1)';
-          thisImg.style.translate = '40% 0%';
-          changeView();
-          console.log(thisImg.offsetHeight);
-          console.log('hi');
+          if (!expanded) {
+            container.style.transform = `scale(1, 1.125)`;
+            container.style.paddingTop = '8%';
+            container.style.paddingBottom = '3%';
+            container.style.marginBottom = '8%';
+            thisImg.style.transform = 'scale(2, 1.125)';
+            thisImg.style.translate = '40% 0%';
+            changeView();
+          } else {
+            container.addEventListener('mousemove', (e) => {
+              // Set up relative zooming
+              const rect = thisImg.getBoundingClientRect();
+              const scaleX = thisImg.offsetWidth / rect.width; // zoom power
+              const scaleY = thisImg.offsetHeight / rect.height;
+              const mouseX = e.clientX;
+              const mouseY = e.clientY;
+
+              // Set up zoom box position and size
+              const zoomSize = 100;
+              const zoomRectangleX = mouseX - zoomSize / 2;
+              const zoomRectangleY = mouseY - zoomSize / 2;
+
+              // Apply to zoom box
+              zoomBox.style.width = zoomSize + 'px';
+              zoomBox.style.height = zoomSize + 'px';
+              // zoomBox.style.transform = `scale(${scaleX}, ${scaleY})`;
+              zoomBox.style.backgroundImage = thisImg.style.backgroundImage;
+              zoomBox.style.backgroundSize = `${rect.width * scaleX}px ${rect.height * scaleY}px`;
+              zoomBox.style.backgroundPosition = `-${mouseX * scaleX}px -${mouseY * scaleY}px`;
+              zoomBox.style.left = zoomRectangleX + 'px';
+              zoomBox.style.top = zoomRectangleY + 'px';
+              zoomBox.style.display = 'block';
+            })
+            thisImg.addEventListener('mouseenter', () => {
+              zoomBox.style.display = 'block';
+            })
+            thisImg.addEventListener('mouseleave', () => {
+              zoomBox.style.display = 'none';
+            })
+          }
         }} />
+      <div id='zoom-box'>
+
+      </div>
     </>
   );
 }

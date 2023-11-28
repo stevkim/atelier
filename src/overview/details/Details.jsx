23eef8905ae520b501d7172b5c69772970748080
Gralayer <wisenathaniel1998@gmail.com>
2023-11-27 19:18:12 -0500

    Fixed issues with the linter in details.

--- src/overview/details/Details.jsx
+++ src/overview/details/Details.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { v4 as key } from 'uuid';
 import './styles.css';
+
 // import ShareButtons from './ShareButtons.jsx';
 
-const Details = ({ product, expanded }) => (
+const Details = ({ product }) => (
   <div className='overview-details' id='overview-details'>
     <h3 className='overview-slogan'>{product.slogan}</h3>
     <p className='overview-description'>{product.description}</p>
     <ul className='overview-feature-wrapper'>
       {product.features.map((feat, index) => (
-        <li className='overview-feature' key={`overview-details-${index}`}>
+        <li className='overview-feature' key={key()}>
           The
           {' '}
           {feat.feature}

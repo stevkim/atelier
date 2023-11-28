5b989e433efb2c6b381c36fe790392498e5dbc09
Gralayer <wisenathaniel1998@gmail.com>
2023-11-27 19:29:48 -0500

    Fixed linter issues with Reviews.jsx.

--- src/overview/info/Reviews.jsx
+++ src/overview/info/Reviews.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import StarRating from '../../components/star-rating/StarRating.jsx';
 import countStarsAndReviews from './helper-funcs/countReviews.js';
 

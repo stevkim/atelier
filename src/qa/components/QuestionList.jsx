fef28f2567eed9161a8ea86cba50b1653ce59bf1
hazelkimberly <hazelkimc@gmail.com>
2023-11-27 20:14:31 -0800

    Use arrow function and replaced curly brackets with parentheses to utilize implicit returns in QuestionList.jsx

--- src/qa/components/QuestionList.jsx
+++ src/qa/components/QuestionList.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import QuestionEntry from './QuestionEntry.jsx';
 
-export default function QuestionList({ currQuestionList, isQuestionExpanded, term }) {
-  return (
-    <div className={`question-list-container ${isQuestionExpanded && 'question-list-container-expanded'}`}>
-      {
-        currQuestionList
-          .filter((question) => (term.toLowerCase() === ''
-            ? question
-            : question.question_body.toLowerCase().includes(term)))
-          .map((question) => <QuestionEntry key={question.question_id} question={question} />)
-      }
-    </div>
-  );
-}
+const QuestionList = ({ currQuestionList, isQuestionExpanded, term }) => (
+  <div className={`question-list-container ${isQuestionExpanded && 'question-list-container-expanded'}`}>
+    {
+      currQuestionList
+        .filter((question) => (term.toLowerCase() === ''
+          ? question
+          : question.question_body.toLowerCase().includes(term)))
+        .map((question) => <QuestionEntry key={question.question_id} question={question} />)
+    }
+  </div>
+);
+
+export default QuestionList;

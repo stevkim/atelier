fe17782ce13a2c2668453b6a6c3eaa50637441d1
Gralayer <wisenathaniel1998@gmail.com>
2023-11-28 13:51:20 -0500

    Fixed API calls.

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
-        <Overview productId={currentProductId} reviewsMetaData={metaData} productInfo={productInfo} />
+        <Overview productId={currentProductId} reviewsMetaData={metaData.data} productInfo={productInfo.data} />
         <RelatedProductsAndComparison
           currentProduct={currentProductId}
           setCurrentProduct={setCurrentProductId}
--- src/overview/Overview.jsx
+++ src/overview/Overview.jsx
@@ -6,14 +6,14 @@ import './styles.css';
 import productExample from './product-example.js';
 import { getOverviewById } from './helper-funcs/axios-requests.js';
 
-const Overview = ({ productId }) => {
+const Overview = ({ productId, reviewsMetaData, productInfo }) => {
   const [product, setProduct] = useState(productExample);
   const [style, setStyle] = useState(0);
   const [inExpandedView, setInExpandedView] = useState(false);
   let selectedStyle = product.styles[style];
 
   useEffect(() => {
-    getOverviewById(productId)
+    getOverviewById(productId, productInfo.data, reviewsMetaData.data)
       .then((res) => setProduct(res))
       .catch((err) => console.log(err));
   }, [productId]);

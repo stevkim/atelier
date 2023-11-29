17a598e0bd378b715c2d3fbf4d48a571c6a516bb
Gralayer <wisenathaniel1998@gmail.com>
2023-11-28 19:54:37 -0500

    Made handling no data coming through more graceful.

--- src/overview/info/Info.jsx
+++ src/overview/info/Info.jsx
@@ -9,9 +9,9 @@ const Info = ({ productInfo, reviewsMetaData, styleInfo, style, selectedStyle, u
   <div aria-label='info' className='overview-overview'>
     <Reviews reviewsMetaData={reviewsMetaData} />
     <div aria-label='category' className='overview-category'>
-      {productInfo.category}
+      {productInfo.category || 'Loading Category...'}
     </div>
-    <h3 aria-label='title' className='overview-title'>{productInfo.name}</h3>
+    <h3 aria-label='title' className='overview-title'>{productInfo.name || 'Loading Title...'}</h3>
     <Price selectedStyle={selectedStyle} includeStyle />
     <StylesView styleIndex={style} styleInfo={styleInfo} updateStyle={updateStyle} />
     <AddToCart skus={selectedStyle.skus || {}} />

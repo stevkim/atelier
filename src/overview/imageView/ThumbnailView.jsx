ae5d03e0ab482042364c736dd99ea0686629083b
Gralayer <wisenathaniel1998@gmail.com>
2023-11-30 13:03:26 -0500

    Exported rotating slice to a helper function.

--- src/overview/imageView/ThumbnailView.jsx
+++ src/overview/imageView/ThumbnailView.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { v4 as key } from 'uuid';
 import Thumbnail from './Thumbnail.jsx';
+import rotatingSlice from '../helper-funcs/rotatingSlice.js';
 
 const ThumbnailView = ({ thumbnails, thumbnail, expanded, updateThumbnail }) => {
   const [topThumbnailIndex, setTopThumbnailIndex] = useState(0);
@@ -15,24 +16,6 @@ const ThumbnailView = ({ thumbnails, thumbnail, expanded, updateThumbnail }) => 
     }
   }, [thumbnail]);
 
-  /*
-    const changeThumbnailIndex = () => {
-      if (topThumbnailIndex + 1 === thumbnails.length) {
-        setTopThumbnailIndex(0);
-      } else {
-        setTopThumbnailIndex(topThumbnailIndex + 1);
-      }
-    };
-  */
-
-  const rotatingSlice = (arr) => {
-    const slice = arr.slice(topThumbnailIndex, Math.min(topThumbnailIndex + sizeOfThumbnailView, thumbnails.length));
-    for (let i = 0; slice.length < Math.min(sizeOfThumbnailView, arr.length); i++) {
-      slice.push(arr[i]);
-    }
-    return slice;
-  };
-
   const incrementThumbnail = (increment) => {
     let newThumbnail = (thumbnail + increment) % Object.keys(thumbnails).length;
     if (newThumbnail < 0) {
@@ -44,7 +27,7 @@ const ThumbnailView = ({ thumbnails, thumbnail, expanded, updateThumbnail }) => 
   return (
     <>
       <div className={`overview-thumbnail-view-${expanded}`}>
-        {rotatingSlice(thumbnails).map((photo, index) => (
+        {rotatingSlice(thumbnails, topThumbnailIndex, sizeOfThumbnailView).map((photo, index) => (
           <Thumbnail
             key={key()}
             url={photo.thumbnail_url}
--- src/overview/info/StylesView.jsx
+++ src/overview/info/StylesView.jsx
@@ -13,6 +13,16 @@ const StylesView = ({ styleIndex, styleInfo, updateStyle }) => (
         index={index}
       />
     ))}
+    <button className='styles-view-up' type='button'>
+      <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-6 h-6'>
+        <path strokeLinecap='round' strokeLinejoin='round' d='M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18' />
+      </svg>
+    </button>
+    <button className='styles-view-down' type='button'>
+      <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-6 h-6'>
+        <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3' />
+      </svg>
+    </button>
   </div>
 );
 
